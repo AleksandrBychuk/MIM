da62cd1f09478300b3641e1363494c9c11367f90
AlexBychuk <sbychuk645@gmail.com>
2020-09-28 16:17:25 +0300

    Did wrapper of application

---
+++ MIM/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MIM.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MIM.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public ICommand CloseApp {
+            get { 
+                return new DelegateCommand((obj) => {
+                    Application.Current.Shutdown();
+                });
+            }
+        }
+    }
+}
